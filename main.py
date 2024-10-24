cf84365c42ac411713e1862640f15e91d88698fe
AP-DAVID <akintolaapre@gmail.com>
2024-10-24 11:44:15 +0100

    added-auth-admin-endpoints

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from sqlalchemy.exc import IntegrityError
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 # Database setup
 SQLALCHEMY_DATABASE_URL = "sqlite:///./portfolio.db"
